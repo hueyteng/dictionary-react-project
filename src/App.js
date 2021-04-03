0a7d7047ac40d3d17ba9e1bd9b1a81dbb7d77d55
hueyteng <78068539+hueyteng@users.noreply.github.com>
2021-04-02 23:14:07 -0700

    Added footer

--- src/App.js
+++ src/App.js
@@ -12,6 +12,13 @@ function App() {
         <Dictionary />
       </main>
       
+    <div className="Footer">
+      Coded by{" "}
+      <a href="https://github.com/hueyteng/dictionary-react-project" target="_blank" rel="noreferrer">
+      Huey Teng Tay
+      </a>
+    </div>
+
       </div>
     </div>
       
