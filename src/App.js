9a5a6164e746dd83e4e0e2d76976be976047fae4
hueyteng <78068539+hueyteng@users.noreply.github.com>
2021-04-02 23:03:54 -0700

    Implemented search form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
-        Yay dictionary!
+        Dictionary
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      
+      </div>
     </div>
+      
   );
 }
 
---
+++ src/Dictionary.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import "./Dictionary.css";
+
+export default function Dictionary() {
+    let [keyword, setKeyword] = useState("");
+
+    function search(event) {
+        event.preventDefault();
+        alert(`Searching for definition of ${keyword}`);
+    }
+
+    function handleKeywordChange(event) {
+        setKeyword(event.target.value);
+    }
+
+    return (
+        <div className="Search">
+            <form onSubmit={search}>
+                <div className="mb-3">
+                    <input 
+                        type="search" 
+                        size="35"
+                        placeholder="Look up a word"
+                        autocomplete="off"
+                        onChange={handleKeywordChange} />
+                <button className="btn btn-info" type="submit" id="search-button">
+                    Search
+                </button>
+                </div>
+            </form>
+        </div>
+    );
+}
