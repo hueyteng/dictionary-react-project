1865599def9aaa4073482aa7bcec22833b417f66
hueyteng <78068539+hueyteng@users.noreply.github.com>
2021-04-02 23:07:19 -0700

    Edited alert message

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -6,7 +6,7 @@ export default function Dictionary() {
 
     function search(event) {
         event.preventDefault();
-        alert(`Searching for definition of ${keyword}`);
+        alert(`Searching for definition of "${keyword}"`);
     }
 
     function handleKeywordChange(event) {
