2dbdb98f9e19b469b5be1faf701a27cbea53fd46
hueyteng <78068539+hueyteng@users.noreply.github.com>
2021-04-08 01:02:07 -0700

    Added Photos section

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,19 +2,32 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./Dictionary.css";
 import Results from "./Results";
+import Photos from "./Photos";
 
 export default function Dictionary(props) {
     let [keyword, setKeyword] = useState(props.defaultKeyword);
     let [results, setResults] = useState(null);
     let [loaded, setLoaded] = useState(false);
+    let [photos, setPhotos] = useState(null);
 
-    function handleResponse(response) {
+    function handleDictionaryResponse(response) {
     setResults(response.data[0]);
+    }
+
+    function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
     }
 
     function search() {
         const apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
-        axios.get(apiUrl).then(handleResponse);
+        axios.get(apiUrl).then(handleDictionaryResponse);
+
+        const pexelsApiKey =
+            "563492ad6f91700001000001b5d9bd6b857a40d8aa2b8bd8392491e0";
+        let  pexelsApiUrl = 
+            `https://api.pexels.com/v1/search?query=${keyword}&per_page=6`;
+        let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+        axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
     }
 
     function handleSubmit(event) {
@@ -45,7 +58,10 @@ export default function Dictionary(props) {
             <div className="card">
             <Results results={results} />
             </div>
-        </div>
+
+            <Photos photos={photos} />
+            </div>
+        
     );
 } else {
     load();
---
+++ src/Photos.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+export default function Photos(props) {
+    if (props.photos) {
+        return (
+            <section className="Photos">
+                <div className="row">
+                {props.photos.map(function (photo, index) {
+                    return (
+                    <div className= "col-6" key={index}>
+                        <a href={photo.src.original} target="_blank" rel="noreferrer">
+                            <div className="HoverImage">
+                                <img src={photo.src.portrait} alt="" className="img-fluid" />
+                            </div>{" "}
+                        </a>
+                        </div>
+                    );
+                })} 
+                </div>
+            </section>
+        );
+    } else {
+        return null;
+}
+}
